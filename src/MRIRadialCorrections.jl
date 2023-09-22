ffa6fbc4cabd6a8e8b2ba9fa3aaa55d522ce7ba6
Felix Horger <felix.horger@kcl.ac.uk>
2023-09-22 21:26:39 +0100

    Added function to compute trajectory shift from time delay

--- src/MRIRadialCorrections.jl
+++ src/MRIRadialCorrections.jl
@@ -11,5 +11,13 @@ module MRIRadialCorrections
 	include("simple.jl")
 	include("ring.jl")
 
+	"""
+		Δk_from_Δt(Δt::Real, dwelltime::Real, num_columns::Integer) = Δk = Δt / dwelltime * 2π / num_columns
+
+	Compute trajectory shift corresponding to a delay.
+
+	"""
+	Δk_from_Δt(Δt::Real, dwelltime::Real, num_columns::Integer) = Δk = Δt / dwelltime * 2π / num_columns
+
 end
 
